d4b48edfc6c6d867dcda2b9252b7268c910d3eee
BUNJEE <vsolomatin@kse.org.ua>
2023-07-14 15:30:10 +0300

    without NetworkX

--- main.py
+++ main.py
@@ -1,33 +1,47 @@
-import networkx as nx
+class Graph:
+    def __init__(self, edges):
+        self.graph = {}
+        for edge in edges:
+            if edge[0] not in self.graph:
+                self.graph[edge[0]] = [edge[1]]
+            else:
+                self.graph[edge[0]].append(edge[1])
 
-# Функція для генерації графа
-def generate_graph(edges):
-    G = nx.Graph()
-    G.add_edges_from(edges)
-    return G
+            if edge[1] not in self.graph:
+                self.graph[edge[1]] = [edge[0]]
+            else:
+                self.graph[edge[1]].append(edge[0])
 
-# Функція для пошуку циклів довжиною >= 3
-def find_cycles(G):
-    cycles = list(nx.simple_cycles(G))
-    valid_cycles = [cycle for cycle in cycles if len(cycle) >= 3]
-    return valid_cycles
+    def find_cycles(self):
+        cycles = []
+        for start_node in self.graph:
+            stack = [(start_node, [start_node])]
+            while stack:
+                node, path = stack.pop()
+                if path and (node == path[0]):
+                    cycles.append(path)
+                    continue
+                for adjacent in self.graph.get(node, []):
+                    if adjacent not in path:
+                        stack.append((adjacent, path + [adjacent]))
+                    else:
+                        if adjacent == path[0] and len(path) >= 3:
+                            stack.append((adjacent, path + [adjacent]))
+        return [cycle for cycle in cycles if len(cycle) >= 3]
 
-# Функція для перевірки, чи всі вершини покриті
-def check_coverage(G, cycles):
-    covered = set()
-    for cycle in cycles:
-        covered.update(cycle)
-    return covered == set(G.nodes)
+    def check_coverage(self, cycles):
+        covered = set()
+        for cycle in cycles:
+            covered.update(cycle)
+        return covered == set(self.graph.keys())
 
-# Функція для вирішення проблеми
 def solve(edges):
-    G = generate_graph(edges)
-    cycles = find_cycles(G)
-    if check_coverage(G, cycles):
+    G = Graph(edges)
+    cycles = G.find_cycles()
+    if G.check_coverage(cycles):
         print("Всі вершини покриті циклами довжиною >= 3")
     else:
         print("Не всі вершини покриті циклами довжиною >= 3")
 
-# Тестуємо функцію на графі
 edges = [(1, 2), (2, 3), (3, 1), (3, 4), (4, 5), (5, 3)]
 solve(edges)
