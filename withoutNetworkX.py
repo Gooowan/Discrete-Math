b5e3b25fbbcc32fe2028d3983781881c06731878
BUNJEE <vsolomatin@kse.org.ua>
2023-07-15 18:42:23 +0300

    finally withoutNetworkX.py done

--- withoutNetworkX.py
+++ withoutNetworkX.py
@@ -1,102 +1,96 @@
+import tkinter as tk
+from tkinter import simpledialog
+from tkinter import messagebox
 import time
 
-# Оголошуємо клас для представлення графу
+# Клас Graph
 class Graph:
-    # Метод ініціалізації графу
-    def __init__(self, edges):
-        # Створюємо порожній словник для графу
-        self.graph = {}
-        # Проходимо по всіх заданих ребрах
-        for edge in edges:
-            # Додаємо кожне ребро до графу
-            if edge[0] not in self.graph:
-                self.graph[edge[0]] = [edge[1]]
-            else:
-                self.graph[edge[0]].append(edge[1])
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[] for _ in range(vertices)]
 
-            # Переконуємося, що граф є ненапрямленим
-            if edge[1] not in self.graph:
-                self.graph[edge[1]] = [edge[0]]
-            else:
-                self.graph[edge[1]].append(edge[0])
+    # Функція для додавання ребра до графа
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
 
-    # Метод для пошуку циклів у графі
-    def find_cycles(self):
-        # Створюємо порожній список для збереження циклів
+    # Функція для знаходження всіх циклів
+    def findCyclesUtil(self, u, parent, visited, recStack, cycles):
+        visited[u] = True
+        recStack[u] = True
+
+        for neighbor in self.graph[u]:
+            if not visited[neighbor]:
+                parent[neighbor] = u
+                self.findCyclesUtil(neighbor, parent, visited, recStack, cycles)
+            elif recStack[neighbor]:
+                cycle = []
+                node = u
+                while node != neighbor:
+                    cycle.append(node)
+                    node = parent[node]
+                cycle.append(node)
+                cycles.append(cycle)
+
+        recStack[u] = False
+
+    # Функція для перевірки, чи всі вершини покриті циклами довжиною >=3
+    def checkCoverage(self):
+        visited = [False] * self.V
+        recStack = [False] * self.V
+        parent = [-1] * self.V
         cycles = []
-        # Проходимо по всіх вершинах графу
-        for start_node in self.graph:
-            # Створюємо стек для DFS
-            stack = [(start_node, [start_node])]
-            # Продовжуємо поки в стеці є елементи
-            while stack:
-                # Беремо вершину зі стеку
-                node, path = stack.pop()
-                # Перевіряємо, чи є шлях циклом
-                if path and (node == path[0]):
-                    cycles.append(path)
-                    continue
-                # Проходимо по всіх сусідніх вершинах
-                for adjacent in self.graph.get(node, []):
-                    # Якщо вершина ще не відвідана, додаємо її до стеку
-                    if adjacent not in path:
-                        stack.append((adjacent, path + [adjacent]))
-                    # Якщо ми знайшли цикл
-                    else:
-                        if adjacent == path[0] and len(path) >= 3:
-                            stack.append((adjacent, path + [adjacent]))
-        # Повертаємо всі цикли з довжиною більше або рівною 3
-        return [cycle for cycle in cycles if len(cycle) >= 3]
 
-    # Метод для перевірки, чи всі вершини покриті циклами
-    def check_coverage(self, cycles):
-        # Створюємо множину для збереження покритих вершин
+        for node in range(self.V):
+            if not visited[node]:
+                self.findCyclesUtil(node, parent, visited, recStack, cycles)
+
+        valid_cycles = [cycle for cycle in cycles if len(cycle) >= 3]
         covered = set()
-        # Проходимо по всіх циклах та додаємо їх до покритих вершин
-        for cycle in cycles:
+        for cycle in valid_cycles:
             covered.update(cycle)
-        # Перевіряємо, чи всі вершини покриті
-        return covered == set(self.graph.keys())
 
+        return covered == set(range(self.V))
 
-# Функція для вирішення задачі
-def solve(edges):
-    # Створюємо граф
-    G = Graph(edges)
-    # Знаходимо цикли в графі
-    cycles = G.find_cycles()
-    # Перевіряємо покриття та виводимо результат
-    if G.check_coverage(cycles):
-        print("Всі вершини покриті циклами довжиною >= 3")
+# Функція для вирішення проблеми
+def solve(size):
+    g = Graph(size)
+    for i in range(size):
+        g.addEdge(i, (i+1)%size)
+        g.addEdge((i+1)%size, (i+2)%size)
+        g.addEdge((i+2)%size, i)
+
+    if g.checkCoverage():
+        return f"Всі вершини покриті циклами довжиною >= 3"
     else:
-        print("Не всі вершини покриті циклами довжиною >= 3")
+        return f"Не всі вершини покриті циклами довжиною >= 3"
 
+# Запуск розв'язку з заданими ребрами
+def run_solve():
+    # Створення кореневого вікна Tkinter
+    root = tk.Tk()
 
-# Створюємо список ребер для тестування
+    # Запит користувача ввести розмір графа
+    size_button = tk.Button(root, text="Введіть розмір графа", command=lambda: get_size_and_solve(root))
+    size_button.pack()
 
-sizes = [10, 100, 1000]
+    root.mainloop()
 
-def generate_edges(n):
-    edges = []
-    # Створюємо цикли довжиною >= 3 для кожної вершини
-    for i in range(n):
-        # Генеруємо цикл з трьох вершин
-        cycle = [(i, (i+1)%n), ((i+1)%n, (i+2)%n), ((i+2)%n, i)]
-        # Додаємо тільки нові ребра до списку
-        for edge in cycle:
-            if edge not in edges and (edge[1], edge[0]) not in edges:
-                edges.append(edge)
-    return edges
+# Функція для отримання розміру та розв'язання
+def get_size_and_solve(root):
+    # Запит користувача ввести розмір графа
+    size = simpledialog.askinteger("Input", "Введіть розмір графа")
 
-# Запускаємо розв'язок з заданими ребрами
-for size in sizes:
-    # Генеруємо граф з випадковими ребрами
-    edges = generate_edges(size)
+    # Перевірка, чи вводив користувач число
+    if size is None:
+        messagebox.showinfo("Вихід", "Розмір не введено, вихід.")
+        return
 
-    # Вимірюємо час виконання алгоритму
+    # Вимірювання часу виконання алгоритму
     start_time = time.time()
-    solve(edges)
+    result = solve(size)
     end_time = time.time()
 
-    # Виводимо час виконання
-    print(f"Розмір графу: {size}, Час виконання: {end_time - start_time} секунди")
+    # Виведення результату та часу виконання у вікні повідомлення
+    messagebox.showinfo("Результат", f"{result}\nРозмір графа: {size}, Час виконання: {end_time - start_time} секунд")
+
+run_solve()
